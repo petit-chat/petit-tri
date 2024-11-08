109e4a641cd0e8d0632958e0613d2148941eb397
petit-chat <76547570+petit-chat@users.noreply.github.com>
2024-11-08 22:13:27 +0100

    refactor: pop-up for sensitive operations + using video tag

--- src/App.svelte
+++ src/App.svelte
@@ -10,9 +10,17 @@
     link.click();
   }
 
+  function reject() {
+    if (confirm("are you sure you want to reject?")) {
+      urls.splice(i, 1);
+    }
+  }
+
   function reset() {
-    urls = [];
-    i = 0;
+    if (confirm("are you sure you want to reset?")) {
+      urls = [];
+      i = 0;
+    }
   }
 
   async function handleFileUpload(event) {
@@ -20,7 +28,6 @@
     if (file) {
       const text = await file.text();
       urls = text.trim().split("\n");
-      i = 0;
     }
   }
 </script>
@@ -28,44 +35,40 @@
 <main>
   {#if urls.length === 0}
     <h1>watch and triage video urls</h1>
-    <input id="file" type="file" onchange={handleFileUpload} aria-label="upload url list" />
+    <input
+      type="file"
+      onchange={handleFileUpload}
+      aria-label="upload url list"
+    />
   {:else if urls.length > i}
     <h1>{urls.length - i} left</h1>
-    <iframe title="video to check" src={urls[i]}></iframe>
+    {#key urls[i]}
+      <!-- svelte-ignore a11y_media_has_caption -->
+      <video autoplay controls width="340">
+        <source src={urls[i]} />
+        video not supported on your browser
+      </video>
+    {/key}
     <div id="sort-btn-grp" class="btn-grp">
-      <button id="delete-btn" onclick={() => urls.splice(i, 1)}>Delete</button>
-      <button id="keep-btn" onclick={() => i++}>Keep</button>
+      <button onclick={reject}>Reject</button>
+      <button onclick={() => i++}>Approve</button>
     </div>
+  {:else if urls.length === i}
+    <h1>done</h1>
   {/if}
   {#if urls.length > 0}
     <div class="btn-grp">
-      <button onclick={reset}>Home</button>
-      <button onclick={save}>Save ({urls.length})</button>
+      <button onclick={reset}>Reset</button>
+      <button onclick={save}>Save ({urls.length} approved)</button>
     </div>
   {/if}
 </main>
 
 <style>
-  h1 {
-    margin-bottom: 5rem;
-  }
-
-  #delete-btn {
-    background-color: red;
-  }
-
-  #keep-btn {
-    background-color: green;
-  }
-
-  #sort-btn-grp,
-  iframe {
-    margin-bottom: 5rem;
-  }
-
   .btn-grp {
     display: flex;
     justify-content: center;
     gap: 1rem;
+    margin-top: 2rem;
   }
 </style>
